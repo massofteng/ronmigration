4c446d4a53fb65a8d6a6e8e7e00533836d6a6284
Sudipto Shakhari <sudipto.shakhari@selise.ch>
2023-10-09 15:03:15 +0600

    update user_details migrations

--- 6_user_details.php
+++ 6_user_details.php
@@ -2,52 +2,93 @@
 include("newdb_conn.php");
 include("olddb_conn.php");
 
-$sql = "SELECT * FROM ro_user_profiles";
+$sql = "SELECT * FROM ro_user_profiles AS profiles LEFT JOIN core_users AS users ON profiles.user_id = users.user_id WHERE 1=1";
 $result = mysqli_query($old_conn, $sql);
 //ini_set('max_execution_time', '300'); //300 seconds = 5 minutes
 ini_set('max_execution_time', '0'); // for infinite time of execution 
 if (mysqli_fetch_array($result) > 0) {
-  while($row = mysqli_fetch_assoc($result)) {
+  while ($row = mysqli_fetch_assoc($result)) {
+    // echo '<pre>';
+    // print_r($row);
+    // echo '</pre>';
 
     $nickname = mysqli_real_escape_string($new_conn, $row['nickname']);
     $user_firstname = mysqli_real_escape_string($new_conn, $row['user_firstname']);
-    $user_lastname = mysqli_real_escape_string($new_conn, $row['sort_name']);
+    $user_surname = mysqli_real_escape_string($new_conn, $row['user_lastname']);
+    $user_profile_type = mysqli_real_escape_string($new_conn, $row['profile_type']);
 
-    $show_name = 0;
-    if($row['show_nickname']=="Y"){
-      $show_name = 1;
+    $short_description = !empty($row['about_me']) ? "'" . mysqli_real_escape_string($new_conn, $row['about_me']) . "'" : "NULL";
+
+    $location = !empty($row['profile_location']) ? "'" . mysqli_real_escape_string($new_conn, $row['profile_location']) . "'" : "NULL";
+    $latitude = !empty($row['google_lat']) ? "'" . mysqli_real_escape_string($new_conn, $row['google_lat']) . "'" : "NULL";
+    $longitude = !empty($row['google_lng']) ? "'" . mysqli_real_escape_string($new_conn, $row['google_lng']) . "'" : "NULL";
+    $language = !empty($row['ron_profile_lang']) ? "'" . mysqli_real_escape_string($new_conn, $row['ron_profile_lang']) . "'" : "NULL";
+    $site = !empty($row['site']) ? "'" . mysqli_real_escape_string($new_conn, $row['site']) . "'" : "NULL";
+    $gender = $row['user_sex'];
+
+    /**
+     * Format user profile type.
+     */
+    if ('company' == $user_profile_type) {
+      $user_profile_type = 'Company';
+    } elseif ('person' == $user_profile_type) {
+      $user_profile_type = 'Individual';
+    }
+
+    /**
+     * Format user gender
+     */
+    if ('m' == $gender) {
+      $gender = 'Male';
+    } elseif ('f' == $gender) {
+      $gender = 'Female';
     }
 
     $insert_sql = "INSERT INTO user_details (
-      `user_id`,
-      `profile_name`, 
-      `profile_id`,
-      `account_type`,
-      `nickname_for_city`,
-      `first_name`,
-      `sur_name`,
-      `created_at`, 
-      `updated_at`
-      )
-    VALUES (
-      '".$row['user_id']."', 
-      '".$nickname."', 
-      '".$row['profile_id']."', 
-      '".$row['profile_type']."', 
-      '". $show_name."', 
-      '". $user_firstname."', 
-      '". $user_lastname."', 
-      '".date('Y-m-d H:i:s', $row['created'])."',
-      '".date('Y-m-d H:i:s', $row['created'])."'
-      )";
-    
+  `user_id`,
+  `profile_id`,
+  `account_type`,
+  `company_name`,
+  `first_name`,
+  `sur_name`,
+  `nickname`,
+  `nickname_for_city`,
+  `gender`,
+  `short_description`,
+  `language`,
+  `location`,
+  `latitude`,
+  `longitude`,
+  `links`,
+  `created_at`, 
+  `updated_at`
+  )
+VALUES (
+  '" . $row['user_id'] . "', 
+  '" . $row['profile_id'] . "', 
+  '" . $user_profile_type . "', 
+  'NULL',
+  '" . $user_firstname . "', 
+  '" . $user_surname . "', 
+  '" . $nickname . "', 
+  0, 
+  '" . $gender . "', 
+  " . $short_description . ", 
+  " . $language . ",
+  " . $location . ", 
+  " . $latitude . ", 
+  " . $longitude . ", 
+  " . $site . ", 
+  '" . date('Y-m-d H:i:s', $row['created']) . "',
+  '" . date('Y-m-d H:i:s', $row['created']) . "'
+  )";
+
     if ($new_conn->query($insert_sql) === TRUE) {
-        echo $row['nickname']. ' '. 'Added</br>';
+      echo $row['nickname'] . ' ' . 'Added</br>';
     } else {
-        //echo "Error: " . $insert_sql . "<br>" . $new_conn->error;
+      //echo "Error: " . $insert_sql . "<br>" . $new_conn->error;
     }
   }
 } else {
   echo "0 results found";
 }
-?>
