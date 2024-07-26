6aa25395f93b6a4015dccef55035743c701760bd
Md Zulficar Hasan Joy <zulficar.hasan@selisegroup.com>
2024-07-26 19:59:08 +0600

    updated with chat assigns

--- 54_user_subscription.php
+++ 54_user_subscription.php
@@ -69,9 +69,9 @@ if ($result->num_rows > 0) {
             $expiration_date = date('Y-m-d H:i:s', $expiration_date);
             if (empty($expiration_date)) {
                 if ($payment_row['profile_upgrade_period'] == "year") {
-                    $expiration_date = date('Y-m-d H:i:s', strtotime('+1 year', $payment_row['created']));
+                    $expiration_date = date('Y-m-d H:i:s', strtotime('+' . $payment_row['profile_upgrade_period_amount'] . ' year', $payment_row['created']));
                 } else {
-                    $expiration_date = date('Y-m-d H:i:s', strtotime('+1 month', $payment_row['created']));
+                    $expiration_date = date('Y-m-d H:i:s', strtotime('+' . $payment_row['profile_upgrade_period_amount'] . ' month', $payment_row['created']));
                 }
             }
             // $expiration_date = date('Y-m-d H:i:s', $expiration_date);
@@ -107,6 +107,56 @@ if ($result->num_rows > 0) {
 
                 if ($new_conn->query($insert_sql) === TRUE) {
                     echo $row['user_id'] . ' ' . 'Added</br>';
+
+                    $packageDetailsSql = "SELECT chat_count, local_chat_count, local_hero_chat_count, local_super_hero_chat_count 
+                                          FROM subscription_package_details 
+                                          WHERE package_id = $package_id 
+                                          LIMIT 1";
+                    $packageResult = mysqli_query($new_conn, $packageDetailsSql);
+                    $packageDetails = mysqli_fetch_assoc($packageResult);
+
+                    $freeChats = isset($packageDetails['chat_count']) ? $packageDetails['chat_count'] : 0;
+                    $localChats = isset($packageDetails['local_chat_count']) ? $packageDetails['local_chat_count'] : 0;
+                    $heroChats = isset($packageDetails['local_hero_chat_count']) ? $packageDetails['local_hero_chat_count'] : 0;
+                    $superHeroChats = isset($packageDetails['local_super_hero_chat_count']) ? $packageDetails['local_super_hero_chat_count'] : 0;
+
+                    // Debugging: Check the values before using them in the SQL query
+                    echo "Debug Info: package_id=$package_id, freeChats=$freeChats, localChats=$localChats, heroChats=$heroChats, superHeroChats=$superHeroChats<br>";
+
+                    $updateOrCreateSql = "INSERT INTO user_chat_assigns (
+                        user_id, profile_id, pause_chat, assign_type, package_id, 
+                        available_free_chat, available_local_chat, available_local_hero_chat, available_super_local_hero_chat, start_date, end_date, status
+                    ) VALUES (
+                        '" . $row['user_id'] . "', 
+                        '" . $row['profile_id'] . "', 
+                        1, 
+                        'package', 
+                        $package_id, 
+                        $freeChats, 
+                        $localChats, 
+                        $heroChats, 
+                        $superHeroChats, 
+                        '" . $start_date . "', 
+                        '" . $expiration_date . "', 
+                        1
+                    ) ON DUPLICATE KEY UPDATE 
+                        pause_chat = 1, 
+                        assign_type = 'package', 
+                        package_id = $package_id, 
+                        available_free_chat = $freeChats, 
+                        available_local_chat = $localChats, 
+                        available_local_hero_chat = $heroChats, 
+                        available_super_local_hero_chat = $superHeroChats, 
+                        start_date = '" . $start_date . "', 
+                        end_date = '" . $expiration_date . "', 
+                        status = 1";
+
+                    if ($new_conn->query($updateOrCreateSql) === TRUE) {
+                        echo $row['user_id'] . ' ' . 'User chat assigned/updated</br>';
+                    } else {
+                        echo "Error: " . $updateOrCreateSql . "<br>" . $new_conn->error;
+                    }
+
                 } else {
                     echo "Error: " . $insert_sql . "<br>" . $new_conn->error;
                 }
